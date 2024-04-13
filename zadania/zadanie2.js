efcd0a92478e9993a4acae88257cb65ff4083fb2
Artur Kuciński <artuyees@gmail.com>
2024-04-13 22:34:49 +0200

    adjust :)

--- zadania/zadanie2.js
+++ zadania/zadanie2.js
@@ -5,24 +5,24 @@ const Zadanie2 = () => {
   const { R, L, F1, F2, U } = zadanie2;
 
   // replace 3.14 with Math.PI
-  const Xl = 2 * 3.14 * F1 * L;
+  const Xl = 2 * Math.PI * F1 * L;
 
-  const Xc = (1 / (2 * 3.14 * F1 * Xl)) * 1000000;
+  const Xc = 1 / (2 * Math.PI * F1 * Xl);
 
-  console.log("Pojemnosc C aby uzyskać dostrojenie: ", Xc + "pF");
+  console.log("Pojemnosc C aby uzyskać dostrojenie: ", Xc + " F");
 
   const I = U / R;
 
   const Uc = (I * Xl) / 1000;
 
-  console.log("Napięcie na kondensatorze dla F1 wynosi: ", Uc + "mV");
+  console.log("Napięcie na kondensatorze dla F1 wynosi: ", Uc + " V");
 
   // Dla F2
 
-  const Xl2 = 2 * 3.14 * F2 * L;
+  const Xl2 = 2 * Math.PI * F2 * L;
 
   // Xc bo amplituda prądu jest taka sama
-  const Xc2 = (1 / (2 * 3.14 * F2 * Xc)) * 1000000;
+  const Xc2 = 1 / (2 * Math.PI * F2 * Xc);
 
   const Z = Math.sqrt(R * R + (Xl2 - Xc2) * (Xl2 - Xc2));
 
@@ -30,7 +30,7 @@ const Zadanie2 = () => {
 
   const Uc2 = J * Xc2;
 
-  console.log("Napięcie na kondensatorze przy F2 wynosi : ", Uc2 + "mV");
+  console.log("Napięcie na kondensatorze przy F2 wynosi : ", Uc2 + " V");
 };
 
 module.exports = Zadanie2;
